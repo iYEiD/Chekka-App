37dfede493c7946976f7e844a9ede82e06571f49
SJR <sammyjoerammouz@gmail.com>
2024-12-03 12:56:41 +0200

    fixed dashboard api service and added snackbars on successful review and cancelation

--- frontend/src/app/ui/modules/pages/dashboard/services/dashboard.service.ts
+++ frontend/src/app/ui/modules/pages/dashboard/services/dashboard.service.ts
@@ -2,12 +2,15 @@ import {inject, Injectable, signal} from '@angular/core';
 import {DashboardApiService} from "./dashboard-api.service";
 import {DashboardViewModel} from "../models/interfaces/dashboard.models";
 import {DashboardMapper} from "../mappers/dashboard.mapper";
+import {SnackbarService} from "../../../../../services/snack-bar/services/snackbar.service";
+import {SnackbarTypeEnums} from "../../../../../services/snack-bar/enum/snackbar-type.enums";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
   dashboardApiService = inject(DashboardApiService)
+  snackbarService = inject(SnackbarService)
   dashboardData = signal<DashboardViewModel | null>(null)
 
   constructor() { }
@@ -23,6 +26,7 @@ export class DashboardService {
   submitReview(bookingId: number, review: any) {
     this.dashboardApiService.submitReview(bookingId, review).subscribe({
       next: res => {
+        this.snackbarService.openSnackBar(SnackbarTypeEnums.SUCCESS, res.message)
         this.fetchDashboardData()
       },
       error: (err) => {}
@@ -32,6 +36,7 @@ export class DashboardService {
   cancelBooking(bookingId: number) {
     this.dashboardApiService.cancelBooking(bookingId).subscribe({
       next: res => {
+        this.snackbarService.openSnackBar(SnackbarTypeEnums.SUCCESS, "Booking cancelled successfully.")
         this.fetchDashboardData()
       },
       error: (err) => {}
